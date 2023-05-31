5599bf1eb72b539f58c03b633ffeae2708ca472f
Dawson Timmons <DawsonTimmons@gmail.com>
2023-05-31 16:23:16 -0600

    create the db schema and migrations

---
+++ db/migrate/20230531193031_create_subscriptions.rb
@@ -0,0 +1,13 @@
+class CreateSubscriptions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :subscriptions do |t|
+      t.references :customers, foreign_key: true
+      t.string :title
+      t.integer :price
+      t.string :status
+      t.string :frequencey
+      
+      t.timestamps
+    end
+  end
+end
