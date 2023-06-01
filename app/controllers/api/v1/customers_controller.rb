ce9e4abe0ecb7bb4998ca7e12fe71c10b33542b6
Dawson Timmons <DawsonTimmons@gmail.com>
2023-06-01 13:08:01 -0600

    add customer controller for endpoint handling

---
+++ app/controllers/api/v1/customers_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::CustomersController < ApplicationController
+  def index
+    render json: CustomerSerializer.new(Customer.all)
+  end
+
+  def show
+    customer = Customer.find(params[:id])
+    begin
+      render json: CustomerSerializer.new(customer)
+    end
+  end
+
+  def create 
+    begin 
+      render json: CustomerSerializer.new(Customer.create!(customer_params))
+    end
+  end
+
+  def update
+    begin
+      render json: CustomerSerializer.new(Customer.update!(params[:id], customer_params))
+    end
+  end
+
+  def destroy
+    begin
+      render json: CustomerSerializer.new(Customer.destroy(params[:id]))
+    end
+  end
+
+private
+  def customer_params
+    params.permit(:first_name, :last_name, :email, :address)
+  end
+end
